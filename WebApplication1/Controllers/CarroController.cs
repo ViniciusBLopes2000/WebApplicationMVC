ededb9aeb0e27fd1478bf75dbeb6d1eaa8e1848d
ViniciusBLopes2000 <viniciusblopes57@gmail.com>
2023-10-04 11:02:28 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Controllers/CarroController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CarroController : Controller
+    {
+        // GET: Carro
+        public ActionResult Index()
+        {
+            return View(CarroModel.CriarLista());
+        }
+
+        // GET: Carro/Details/5
+        public ActionResult Details(int id)
+        {
+            var carro = CarroModel.CriarLista()[id];
+            return View(carro);
+
+
+        }
+
+        // GET: Carro/Create
+        public ActionResult Create()
+        {
+            var carro = CarroModel.CriarCarro();
+            return View();
+        }
+
+        // POST: Carro/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Carro/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var carro = CarroModel.CriarLista()[id];
+            return View(carro);
+        }
+
+        // POST: Carro/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Carro/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var carro = CarroModel.CriarLista()[id];
+            return View(carro);
+        }
+
+        // POST: Carro/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/CarroModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CarroModel
+    {
+        public string Modelo { get; set; }
+        public string Dono { get; set; }
+        public string Placa { get; set; }
+        public int Ano { get; set; }
+
+        public CarroModel()
+        {
+            Modelo = "Mitsubish - Lancer";
+            Dono = "Emerson da Silva Rosa";
+            Placa = "DEF-5678";
+            Ano = 2020;
+        }
+
+        public static CarroModel CriarCarro()
+        {
+            var carro = new CarroModel();
+            carro.Modelo = "Porsche - Huracan";
+            return carro;
+        }
+        public static List<CarroModel> CriarLista()
+        {
+            var lista = new List<CarroModel>();
+
+            lista.Add(new CarroModel() { Modelo = "Toyota - Corolla", Dono = "Vinícius Bezerra Lopes", Placa = "XYZ-9876", Ano = 2010 });
+            lista.Add(new CarroModel() { Modelo = "Fiat - Cronos ", Dono = "Alex Machado Ferreira Sampaio", Placa = "MNO-5432", Ano = 2021 });
+            lista.Add(new CarroModel() { Modelo = "Jeep - Compass ", Dono = " Maria Eduarda de Azevedo ", Placa = "QRS-1234", Ano = 2022 });
+            lista.Add(new CarroModel() { Modelo = "Fiat - Toro", Dono = "Marco Aurélio de Barros ", Placa = "LMN-7890", Ano = 2020 });
+            lista.Add(new CarroModel() { Modelo = "Honda - Civic ", Dono = "Bianca Grazielly de Lima", Placa = "GHI-4567", Ano = 2017 });
+            lista.Add(new CarroModel() { Modelo = "Ford - Ka ", Dono = "Erik Paschoine de Oliveira ", Placa = "UVW-2109", Ano = 2011 });
+
+            return lista;
+        }
+    }
+}
